463e73c08117ebd1157e79ee04aee54f703ab122
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-26 16:35:31 +0100

    feat: added StartScreen

---
+++ src/views/StartScreen/StartScreen.js
@@ -0,0 +1,11 @@
+import './StartScreen.css'
+const StartScreen = () => {
+    return ( 
+        <div>
+            <h1>Simon Palmgren</h1>
+            <p>Simon Palmgren</p>
+        </div>
+    );
+}
+ 
+export default StartScreen;
