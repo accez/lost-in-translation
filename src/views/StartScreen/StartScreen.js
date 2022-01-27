d683db0cfe2b407878727fc9924d92ba435bd1d8
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-27 14:01:13 +0100

    feat: styling and markup completed for startscreen

--- src/views/StartScreen/StartScreen.js
+++ src/views/StartScreen/StartScreen.js
@@ -1,11 +1,26 @@
-import './StartScreen.css'
+import './StartScreen.css';
+import logo from '../../assets/Logo-Hello.png';
+import splash from '../../assets/Splash.svg';
 const StartScreen = () => {
-    return ( 
-        <div>
-            <h1>Simon Palmgren</h1>
-            <p>Simon Palmgren</p>
+  return (
+    <div className="start-screen-body">
+      <div className="container">
+        <div className="logo-container">
+          <div>
+            <img className="logo" src={logo}></img>
+            <img className="splash" src={splash}></img>
+          </div>
+          <div className="text">
+            <h1>Lost in Translation</h1>
+            <h2>Get started</h2>
+          </div>
         </div>
-    );
-}
- 
+        <div className="box-container">
+          <div className="card"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default StartScreen;
