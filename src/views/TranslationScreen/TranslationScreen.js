7d03e0402442b30e5321cad45a137bc9a1f1fcb8
Love Beling <love.beling@se.experis.com>
2022-01-27 16:10:25 +0100

    feat: TranslationScreen now shows the sign language display card

--- src/views/TranslationScreen/TranslationScreen.js
+++ src/views/TranslationScreen/TranslationScreen.js
@@ -1,6 +1,11 @@
 import './TranslationScreen.css';
+import SignLanguageCard from './components/SignLanguageCard';
 const TranslationScreen = () => {
-  return <div className="translation-screen-body"></div>;
+  return (
+    <div className="translation-screen-body">
+      <SignLanguageCard />
+    </div>
+  );
 };
 
 export default TranslationScreen;
