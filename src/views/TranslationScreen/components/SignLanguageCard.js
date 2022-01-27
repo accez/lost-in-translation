339f6eb67d4d5a3cd756223b35f419d61e1484b7
Love Beling <love.beling@se.experis.com>
2022-01-27 16:11:27 +0100

    feat: Created different components used for displaying the translated sign language

---
+++ src/views/TranslationScreen/components/SignLanguageCard.js
@@ -0,0 +1,10 @@
+import './SignLanguageCard.css';
+const SignLanguageCard = () => {
+  return (
+    <div className="box-container">
+      <div className="card"></div>
+    </div>
+  );
+};
+
+export default SignLanguageCard;
