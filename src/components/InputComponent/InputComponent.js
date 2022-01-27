0c5f11ad3d3980a74c084b5d40efccba5987cb67
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-27 16:20:02 +0100

    fix: InputComponent can now be used everywhere

--- src/components/InputComponent/InputComponent.js
+++ src/components/InputComponent/InputComponent.js
@@ -1,22 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './InputComponent.css';
 
 import keyboard from '../../assets/outline_keyboard_black_24dp.png';
 
-const InputComponent = ({ placeholder }) => {
-  const [input, setInput] = useState('');
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    return input;
-  };
-
+const InputComponent = ({ placeholder, handleSubmit, handleOnChange }) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
           <img className="keyboard-img" src={keyboard}></img>
-          <input type="text" onChange={(e) => setInput(e.target.value)} placeholder={placeholder} />
+          <input type="text" onChange={handleOnChange} placeholder={placeholder} />
         </label>
         <button type="submit">&rarr; </button>
       </form>
