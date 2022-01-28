0e6eb749fe4c965c806bf3b3c64419ffb0493f05
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-28 16:53:56 +0100

    Feat: Can now upload user

--- src/views/StartScreen/StartScreen.js
+++ src/views/StartScreen/StartScreen.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import InputComponent from '../../components/InputComponent/InputComponent';
-import useFetch from '../../helpers/useFetch';
 import { config } from '../../helpers/config';
 
 import logo from '../../assets/Logo-Hello.png';
@@ -9,8 +8,33 @@ import splash from '../../assets/Splash.svg';
 import './StartScreen.css';
 
 const StartScreen = () => {
-  const res = useFetch(`${config.url}/translations`, {});
-  console.log(res.response);
+  const [userInput, setUserInput] = useState('');
+
+  const handleSubmit = async (event) => {
+    const uniq = new Date().getTime();
+    event.preventDefault();
+    try {
+      let response = await fetch(`${config.url}/translations`, {
+        method: 'POST',
+        headers: {
+          'X-API-Key': config.apiKey,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          id: uniq,
+          username: userInput,
+          translations: []
+        })
+      });
+      await response.json();
+    } catch (error) {
+      console.error(console.error());
+    }
+  };
+
+  const getUserName = (event) => {
+    setUserInput(event.target.value);
+  };
 
   return (
     <div className="start-screen-body">
@@ -27,7 +51,11 @@ const StartScreen = () => {
         </div>
         <div className="box-container">
           <div className="card">
-            <InputComponent placeholder="What is your name?" />
+            <InputComponent
+              placeholder="What is your name?"
+              handleSubmit={handleSubmit}
+              handleOnChange={getUserName}
+            />
           </div>
         </div>
       </div>
