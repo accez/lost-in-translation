37959fe28eff6adc4c1017bff1e788dd9a6f59ed
Love Beling <love.beling@se.experis.com>
2022-01-28 08:56:56 +0100

    feat: added logic for filtering inputed string

--- src/views/TranslationScreen/components/SignList.js
+++ src/views/TranslationScreen/components/SignList.js
@@ -2,16 +2,16 @@ import SignItem from './SignItem';
 import { images } from './SignLanguageImages';
 const SignList = () => {
   //shghehhnhhsbhahhhphihuhhkhhhh
-  const strArray = 'hswheghhkhhshghehhnhhsbha';
-  const stringArray = strArray.split('');
-  console.log(stringArray);
-
+  const input = 'hswh345egh2hkhhsh3gheh4hnhh4s - bhashgheh5  "hnhhsbhahhhph , ihuhhkhhhh';
+  const strippedInput = input.replace(/[^a-zA-Z]+/g, '');
+  const inputChars = strippedInput.split('');
+  console.log(inputChars);
   return (
-    <div>
-      {stringArray.map((char, index) => (
+    <>
+      {inputChars.map((char, index) => (
         <SignItem signImage={images[char]} key={index} />
       ))}
-    </div>
+    </>
   );
 };
 
