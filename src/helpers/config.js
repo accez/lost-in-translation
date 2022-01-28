5c29df01899d4010356e05b19878663bfbaa9b66
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-01-28 15:15:28 +0100

    fix: url now dont end with "/"

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,4 @@
 export const config = {
   apiKey: 'X9dHGcSU9kuwKyxz2/p+TA==',
-  url: 'https://spa-lb-experis-assignment.herokuapp.com/'
+  url: 'https://spa-lb-experis-assignment.herokuapp.com'
 };
