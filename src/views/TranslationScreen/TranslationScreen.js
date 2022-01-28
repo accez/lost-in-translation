b2b0c419ef3fdd834ff43687040bd580f87447c5
Love Beling <love.beling@se.experis.com>
2022-01-28 09:37:40 +0100

    feat: added input component to translation screen

--- src/views/TranslationScreen/TranslationScreen.js
+++ src/views/TranslationScreen/TranslationScreen.js
@@ -1,8 +1,12 @@
 import './TranslationScreen.css';
 import SignLanguageCard from './components/SignLanguageCard';
+import InputComponent from '../../components/InputComponent/InputComponent';
 const TranslationScreen = () => {
   return (
     <div className="translation-screen-body">
+      <div className="input">
+        <InputComponent placeholder="Translate to sign language" />
+      </div>
       <SignLanguageCard />
     </div>
   );
