87220ccde0d57b8c18d6a0f90fbd9ef88d26901b
Love Beling <love.beling@se.experis.com>
2022-01-31 10:10:16 +0100

    feat: created profile screen, and added some css

---
+++ src/views/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,11 @@
+import './ProfileScreen.css';
+
+function ProfileScreen() {
+  return (
+    <div className="profile-screen-body">
+      <h1 className="logged-in-user">Logged in user</h1>
+    </div>
+  );
+}
+
+export default ProfileScreen;
