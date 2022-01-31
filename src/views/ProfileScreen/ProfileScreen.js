a3d8ad60a41660a0322d9acc588426df714959f2
Love Beling <love.beling@se.experis.com>
2022-01-31 14:28:07 +0100

    feat:(ProfileScreen) implemented logic for fetching ten latest translations, aswell as logic for deleting translations

--- src/views/ProfileScreen/ProfileScreen.js
+++ src/views/ProfileScreen/ProfileScreen.js
@@ -1,9 +1,79 @@
+import { useEffect, useState } from 'react';
 import './ProfileScreen.css';
 
 function ProfileScreen() {
+  const [translations, SetTranslations] = useState([]);
+  useEffect(async () => {
+    const response = await fetch('https://spa-lb-experis-assignment.herokuapp.com/translations/1');
+    const json = await response.json();
+    const activeTranslations = json.translations.filter((translation) => {
+      return translation.deleted !== true;
+    });
+
+    const addProp = [];
+    json.translations.forEach((trans, index) => {
+      addProp[index] = {
+        translation: trans,
+        deleted: false
+      };
+    });
+    console.log(addProp);
+    console.log(activeTranslations);
+    SetTranslations(activeTranslations.slice(-10));
+  }, []);
+
+  async function DeleteTranslation(index) {
+    const response = await fetch('https://spa-lb-experis-assignment.herokuapp.com/translations/1');
+    const json = await response.json();
+
+    json.translations[index] = {
+      translation: json.translations[index],
+      deleted: true
+    };
+
+    const apiURL = 'https://spa-lb-experis-assignment.herokuapp.com';
+    const apiKey = 'X9dHGcSU9kuwKyxz2/p+TA==';
+    const userId = 1; // Update user with id 1
+
+    fetch(`${apiURL}/translations/${userId}`, {
+      method: 'PATCH', // NB: Set method to PATCH
+      headers: {
+        'X-API-Key': apiKey,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        // Provide new translations to add to user with id 1
+        translations: json.translations
+      })
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Could not update translations history');
+        }
+        return response.json();
+      })
+      .then((updatedUser) => {
+        console.log(updatedUser);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }
   return (
     <div className="profile-screen-body">
       <h1 className="logged-in-user">Logged in user</h1>
+      {translations.map((translation, index) => (
+        <li key={index}>
+          {translation}
+          <button
+            onClick={() => {
+              DeleteTranslation(index);
+            }}>
+            Delete
+          </button>
+        </li>
+      ))}
     </div>
   );
 }
