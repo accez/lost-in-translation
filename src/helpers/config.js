aa7c857616a0f064c7f03468c8e04a7417f83c58
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-02-01 10:53:35 +0100

    fix: removed "/" from url in config to fix issues

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -3,5 +3,5 @@
  */
 export const config = {
   apiKey: 'X9dHGcSU9kuwKyxz2/p+TA==',
-  url: 'https://spa-lb-experis-assignment.herokuapp.com/'
+  url: 'https://spa-lb-experis-assignment.herokuapp.com'
 };
